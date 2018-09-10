cc94de1b04676a4d5f03aaa6768d266be4928b0c
Jan Cajthaml <jan.cajthaml@gmail.com>
2018-09-10 22:19:54 +0200

    debian improvements (#43)

--- bbtest/spec.rb
+++ bbtest/spec.rb
@@ -34,7 +34,7 @@ RSpec.configure do |config|
       return ($? == 0 ? containers.split("\n") : [])
     end
 
-    ids = %x(systemctl list-units | awk '{ print $1 }')
+    ids = %x(systemctl -a -t service --no-legend | awk '{ print $1 }')
 
     if $?
       ids = ids.split("\n").map(&:strip).reject { |x|
