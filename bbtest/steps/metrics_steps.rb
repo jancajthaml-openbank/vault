123c8689b93295ae2fb91fb566c0dd75f4955e81
Jan Cajthaml <jan.cajthaml@gmail.com>
2018-10-02 23:09:27 +0200

    metrics fixed, hangs during bootstrap fixed

--- bbtest/steps/metrics_steps.rb
+++ bbtest/steps/metrics_steps.rb
@@ -5,8 +5,8 @@ require 'json'
 step "metrics for tenant :tenant should report :count created accounts" do |tenant, count|
   metrics_file = "/opt/vault/metrics/metrics.#{tenant}.json"
 
-  expect(File.file?(metrics_file)).to be(true)
   eventually(timeout: 3) {
+    expect(File.file?(metrics_file)).to be(true)
     metrics = File.open(metrics_file, 'rb') { |f| JSON.parse(f.read) }
     expect(metrics["createdAccounts"]).to eq(count)
   }
