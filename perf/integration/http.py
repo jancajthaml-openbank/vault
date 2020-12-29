bf63d6e8862e44481a85c768d272acb9e7c889f3
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-12-29 12:35:53 +0100

    wip

--- perf/integration/http.py
+++ perf/integration/http.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
 import ssl
 import urllib.request
 import socket
@@ -13,12 +12,14 @@ class Integration(object):
     self.__endpoint = 'https://127.0.0.1'
 
   def create_account(self, tenant, name):
-    payload = {
-      'name': name,
-      'format': 'perf',
-      'currency': 'CZK',
-      'isBalanceCheck': False,
-    }
+    payload = """
+      {
+        "name": "{}",
+        "format": "perf",
+        "currency": "CZK",
+        "isBalanceCheck": false
+      }
+    """.format(name)
     uri = "{}/account/{}".format(self.__endpoint, tenant)
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
@@ -26,7 +27,7 @@ class Integration(object):
     request = urllib.request.Request(method='POST', url=uri)
     request.add_header('Accept', 'application/json')
     request.add_header('Content-Type', 'application/json')
-    request.data = json.dumps(payload).encode('utf-8')
+    request.data = payload.encode('utf-8')
     try:
       response = urllib.request.urlopen(request, timeout=10, context=ctx)
     except socket.timeout:
