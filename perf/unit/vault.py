ffc6af2afe7d4c5b5dc568e68f1a6635a567aef5
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-10-30 08:28:44 +0200

    bbtests human readable subprocess signals

--- perf/unit/vault.py
+++ perf/unit/vault.py
@@ -27,7 +27,7 @@ class Vault(object):
       (code, result, error) = execute([
         'journalctl', '-o', 'cat', '-u', '{}.service'.format(unit), '--no-pager'
       ], True)
-      if code != 0 or not result:
+      if code != 'OK' or not result:
         continue
       filename = os.path.realpath('{}/../../reports/perf-tests/logs/{}.log'.format(os.path.dirname(os.path.abspath(__file__)), unit))
       with open(filename, 'w') as f:
@@ -37,10 +37,10 @@ class Vault(object):
     unit = 'vault-unit@{}'.format(tenant)
 
     (code, result, error) = execute(["systemctl", 'enable', unit])
-    assert code == 0, str(result) + ' ' + str(error)
+    assert code == 'OK', str(result) + ' ' + str(error)
 
     (code, result, error) = execute(["systemctl", 'start', unit])
-    assert code == 0, str(result) + ' ' + str(error)
+    assert code == 'OK', str(result) + ' ' + str(error)
 
     self.tenants.append(tenant)
 
@@ -49,7 +49,7 @@ class Vault(object):
       (code, result, error) = execute([
         "systemctl", "show", "-p", "SubState", unit
       ])
-      assert code == 0, str(result) + ' ' + str(error)
+      assert code == 'OK', str(result) + ' ' + str(error)
       assert 'SubState=running' in result
     wait_for_running()
 
@@ -58,14 +58,14 @@ class Vault(object):
 
     for unit in units:
       (code, result, error) = execute(['systemctl', 'restart', unit])
-      assert code == 0, str(result) + ' ' + str(error)
+      assert code == 'OK', str(result) + ' ' + str(error)
 
       @eventually(30)
       def wait_for_running():
         (code, result, error) = execute([
           "systemctl", "show", "-p", "SubState", unit
         ])
-        assert code == 0, str(result) + ' ' + str(error)
+        assert code == 'OK', str(result) + ' ' + str(error)
         assert 'SubState=running' in result
       wait_for_running()
 
@@ -74,20 +74,20 @@ class Vault(object):
 
     for unit in units:
       (code, result, error) = execute(['systemctl', 'stop', unit])
-      assert code == 0, str(result) + ' ' + str(error)
+      assert code == 'OK', str(result) + ' ' + str(error)
 
   def start(self) -> bool:
     units = ['vault-rest'] + ['vault-unit@{}'.format(tenant) for tenant in self.tenants]
 
     for unit in units:
       (code, result, error) = execute(['systemctl', 'start', unit])
-      assert code == 0, str(result) + ' ' + str(error)
+      assert code == 'OK', str(result) + ' ' + str(error)
 
       @eventually(30)
       def wait_for_running():
         (code, result, error) = execute([
           "systemctl", "show", "-p", "SubState", unit
         ])
-        assert code == 0, str(result) + ' ' + str(error)
+        assert code == 'OK', str(result) + ' ' + str(error)
         assert 'SubState=running' in result
       wait_for_running()
